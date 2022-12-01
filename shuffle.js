6eff0673247d6bd674c7a60450c6111ab266b4c9
fairyaksh <akshayasatheesh@hotmail.co.uk>
2022-12-01 17:21:44 +0000

    Adding helper function that shuffles an array of strings

---
+++ shuffle.js
@@ -0,0 +1,23 @@
+// Helper function using Fisher-Yates algorithm:
+// https://stackoverflow.com/a/2450976/1293256
+
+function shuffle (array) {
+
+	let currentIndex = array.length;
+	let temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+}
